c7b197f0c2abe5e99209bf8ca2361c5513dcfb34
abraham sofer <alexbodn@gmail.com>
2024-07-26 10:42:00 +0200

    streamline

--- db_header.c
+++ db_header.c
@@ -1,2 +1,6 @@
+/*
+example to achieve BDB_HEADER
+BDB_HEADER=$(cat db_header.c |cc -I$TERMUX_PREFIX/include -P -E -| tail -n 1| sed -e 's/[\ \t]//g')
+*/
 #include <db.h>
 DB_VERSION_MAJOR.DB_VERSION_MINOR
